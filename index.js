59e1ee82ad6585b6887a4401775a7cfbc2b5cbd0
Jamari556 <Jamari.Greene25@compscihigh.org>
2025-02-28 05:47:32 +0000

    done

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ const app = express();
 
 // Task 1: Create your views folder and add the correct files to it. 
 
+
+
+
 // Task 2: Set the view engine to ejs.
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  // Task 3: Create a data object with the following 3 keys:
-  // "title" key with the a string as a value (Pick Your Store's title)
-  // "itemsOnSale" key with a boolean indicating whether you have items on sale.
-  // "products" key with an array of at least 3 objects
-  // Each Object in "products" should have 2 keys
-  // "name" key with a string as a value (Name the item your selling)
-  // "sale" key with a string as a value (Give it a sale, like 5% off. If there is no sale leave an empty string)
-
-
-
-  // Task 4: Render the ejs file "store.ejs" with your data object.
-});
-
+  const data = {
+    title: "Balenci Store",
+    itemsOnSale: true,
+    products: [
+      { name: "Runners", sale: "10% off" },
+      { name: "Handbag", sale: "5" },
+      { name: "Tracks", sale: "15% off" }
+    ]
+  };
 // Task 5: [CONTINUE TASKS IN store.ejs]
 
-app.listen(() => {
-  console.log(`Server is running.`);
+res.render("store.ejs", data);
+});
+
+app.listen(3000, () => {
+  console.log("Balenci is runnin");
 });
